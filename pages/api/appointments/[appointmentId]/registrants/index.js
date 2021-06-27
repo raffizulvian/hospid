7811fb507343d68f5957e6ff2c2b558cb32fd84c
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-28 00:06:53 +0700

    feat(api): add GET method for /appointments/[id]/registrants API

---
+++ pages/api/appointments/[appointmentId]/registrants/index.js
@@ -0,0 +1,20 @@
+import AppointmentModel from '../../../../../lib/server/models/appointmentModel';
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case 'GET': {
+      const { appointmentId } = req.query;
+
+      try {
+        const data = await AppointmentModel.getRegistrants(appointmentId);
+        res.status(200).json(data);
+      } catch (err) {
+        res.status(err.code).json({ message: err.message });
+      }
+      break;
+    }
+
+    default:
+      res.status(405).end();
+  }
+}
