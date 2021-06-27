4bba9b5b2df14db2087d65788b88acd5fa0f6346
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-27 21:10:44 +0700

    feat(api): add PUT method for /appointments/[id] API

---
+++ pages/api/appointments/[appointmentId]/index.js
@@ -0,0 +1,23 @@
+import AppointmentModel from '../../../../lib/server/models/appointmentModel';
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case 'PUT': {
+      const { appointmentId } = req.query;
+      const { doctorName, description, capacity } = req.body;
+
+      try {
+        const id = await AppointmentModel.update(appointmentId, doctorName, description, capacity);
+        res.status(201).json({ id });
+      } catch (err) {
+        console.log(err);
+        res.status(err.code).json({ message: err.message });
+      }
+      break;
+    }
+
+    default:
+      res.status(405).end();
+      break;
+  }
+}
