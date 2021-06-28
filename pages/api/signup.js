27042d81be5f085697a9b9cc1173ecfcd2948e30
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-28 17:08:00 +0700

    fix(api): remove user access to signup as admin

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -3,11 +3,10 @@ import User from '../../lib/server/models/userModel';
 export default async function handler(req, res) {
   switch (req.method) {
     case 'POST': {
-      const { type } = req.query;
       const { firstName, lastName, age, email, username, password } = req.body;
 
       try {
-        const data = await User.signup(firstName, lastName, age, email, username, password, type);
+        const data = await User.signup(firstName, lastName, age, email, username, password);
 
         res.status(200).json(data);
       } catch (err) {
