308575ebdcf84510e94659f8c4cc8166c3490a13
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-28 16:56:19 +0700

    fix(api): allow admin to signup

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -3,10 +3,11 @@ import User from '../../lib/server/models/userModel';
 export default async function handler(req, res) {
   switch (req.method) {
     case 'POST': {
+      const { type } = req.query;
       const { firstName, lastName, age, email, username, password } = req.body;
 
       try {
-        const data = await User.signup(firstName, lastName, age, email, username, password);
+        const data = await User.signup(firstName, lastName, age, email, username, password, type);
 
         res.status(200).json(data);
       } catch (err) {
