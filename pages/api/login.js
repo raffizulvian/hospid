b1101007a90bc9c3953a89cdced9959378e33a18
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-29 02:01:09 +0700

    feat: add login authentication system

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,8 @@
+import { serialize } from 'cookie';
 import User from '../../lib/server/models/userModel';
+
+const fifteenMinutes = 15 * 60;
+const oneMonth = 4 * 7 * 24 * 60 * 60;
 
 export default async function handler(req, res) {
   switch (req.method) {
@@ -6,10 +10,29 @@ export default async function handler(req, res) {
       const { type } = req.query;
       const { username, password } = req.body;
 
-      try {
-        const data = await User.login(username, password, type);
+      const accessOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: fifteenMinutes,
+        path: '/',
+      };
 
-        res.status(200).json(data);
+      const refreshOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: oneMonth,
+        path: '/',
+      };
+
+      try {
+        const response = await User.login(username, password, type);
+
+        res.setHeader('Set-Cookie', serialize('token', response.token.accessToken, accessOptions));
+        res.setHeader('Set-Cookie', serialize('RFSTKN', response.token.refreshToken, refreshOptions));
+
+        res.status(200).json(response.data);
       } catch (err) {
         res.status(err.code || 500).json({ message: err.message });
       }
