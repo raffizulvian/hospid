dc39f1484ad6bcf0515cb144cc529018416a82e4
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-29 03:09:54 +0700

    refactor: handle cookie on seperate file
    also fix bugs that make cookie only one stored

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,8 +1,5 @@
-import { serialize } from 'cookie';
 import User from '../../../lib/server/models/userModel';
-
-const fifteenMinutes = 15 * 60;
-const oneMonth = 4 * 7 * 24 * 60 * 60;
+import { setToken } from '../../../lib/server/utils/cookies';
 
 export default async function handler(req, res) {
   switch (req.method) {
@@ -10,27 +7,11 @@ export default async function handler(req, res) {
       const { type } = req.query;
       const { username, password } = req.body;
 
-      const accessOptions = {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: fifteenMinutes,
-        path: '/',
-      };
-
-      const refreshOptions = {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: oneMonth,
-        path: '/',
-      };
-
       try {
         const response = await User.login(username, password, type);
 
-        res.setHeader('Set-Cookie', serialize('token', response.token.accessToken, accessOptions));
-        res.setHeader('Set-Cookie', serialize('RFSTKN', response.token.refreshToken, refreshOptions));
+        const cookies = setToken(response.token.accessToken, response.token.refreshToken);
+        res.setHeader('Set-Cookie', cookies);
 
         res.status(200).json(response.data);
       } catch (err) {
