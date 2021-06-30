31864e2bf5c8aa4a4ed37e67eb399b5549e73621
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-01 01:19:43 +0700

    fix(api): set token to cookies

--- pages/api/refresh_token.js
+++ pages/api/refresh_token.js
@@ -1,9 +1,12 @@
+import withCookie from '../../lib/server/middleware/withCookie';
+import withToken from '../../lib/server/middleware/withToken';
 import Token from '../../lib/server/models/tokenModel';
 
 async function handler(req, res) {
   switch (req.method) {
     case 'POST': {
       const refreshToken = req.cookies.RFSTKN;
+
       if (typeof refreshToken === 'undefined') {
         res.status(401).end();
         break;
@@ -11,11 +14,12 @@ async function handler(req, res) {
 
       try {
         const token = Token.refresh(refreshToken);
-        res.status(200).json(token);
+
+        res.token(token.accessToken, token.refreshToken);
+        res.status(200).end();
       } catch (err) {
         res.status(err.code || 500).json({ message: err.message });
       }
-
       break;
     }
     default:
@@ -24,4 +28,4 @@ async function handler(req, res) {
   }
 }
 
-export default handler;
+export default withCookie(withToken(handler));
