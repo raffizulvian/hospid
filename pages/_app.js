cbc4aebdd89bae0b3d0bd1fd65054848a5f5c2a6
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-08 00:47:03 +0700

    refactor: wrap pages with Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  if (Component.withNavbar) {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
+
   return <Component {...pageProps} />;
 }
 
