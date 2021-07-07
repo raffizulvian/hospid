0434b3ff8f63f2b598924e6f7560c595bd9dcbd0
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-08 00:45:44 +0700

    feat(components): add ButtonAction component

---
+++ components/ButtonAction.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+function ButtonAction({ children, id, onClick, secondary, big, className }) {
+  return (
+    <button
+      id={id}
+      onClick={onClick}
+      className={`inline-block text-center font-semibold rounded-lg border border-solid m-0 ${
+        secondary
+          ? 'bg-white text-purple-600 border-solid border-purple-600'
+          : 'bg-purple-600 text-white'
+      } ${
+        big
+          ? 'h-14 min-w-[10rem] text-lg px-6 py-3 md:h-[3.875rem] md:min-w-[12.25rem] md:text-xl md:px-7 md:py-4'
+          : 'h-10 min-w-[6rem] text-sm px-3 py-2'
+      } ${className}`}>
+      {children}
+    </button>
+  );
+}
+
+ButtonAction.propTypes = {
+  children: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  secondary: PropTypes.bool,
+  big: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default ButtonAction;
