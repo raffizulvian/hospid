c1e4b8387f1f92c6f7ef6b781742533207ef8762
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-08 00:44:35 +0700

    feat(components): add Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import ButtonLink from './ButtonLink';
+
+function Navbar({ children }) {
+  return (
+    <header className='sticky top-0 h-[4.5rem] w-full px-[4%] py-4 shadow-md z-30 bg-white lg:px-0'>
+      <div className='flex items-center h-full w-full max-w-screen-lg mx-auto'>
+        <div className='text-lg font-bold md:mr-6'>
+          {/* Logo */}
+          Hospid
+        </div>
+        <nav className='flex justify-between items-center h-full w-full'>
+          <div className='flex items-center md:w-2/5 space-x-8'>
+            <div className='hidden md:flex justify-around items-center w-full'>{children}</div>
+          </div>
+          <div className='hidden md:flex items-center space-x-4'>
+            <ButtonLink href='/login' secondary>
+              Login
+            </ButtonLink>
+            <ButtonLink href='/signup'>Sign up</ButtonLink>
+          </div>
+          <ButtonLink className='md:hidden' href='/login' secondary>
+            Login
+          </ButtonLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+Navbar.propTypes = { children: PropTypes.arrayOf(PropTypes.element).isRequired };
+
+export default Navbar;
