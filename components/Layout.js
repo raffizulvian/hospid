036cfa6d8db2bf450c8243ef00fb790950fff174
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-08 00:46:11 +0700

    feat(components): add Layout component

---
+++ components/Layout.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import BurgerMenu from './BurgerMenu';
+import Navbar from './Navbar';
+import NavLink from './NavLink';
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar>
+        <NavLink href='/' color='text-gray-900'>
+          Home
+        </NavLink>
+        <NavLink href='/konsultasi' color='text-gray-900'>
+          Konsultasi
+        </NavLink>
+        <NavLink href='/tentang' color='text-gray-900'>
+          Tentang
+        </NavLink>
+        <NavLink href='/faq' color='text-gray-900'>
+          FAQ
+        </NavLink>
+      </Navbar>
+      <BurgerMenu>
+        <Link href='/'>
+          <a>Hospid</a>
+        </Link>
+        <Link href='/konsultasi'>
+          <a>Konsultasi</a>
+        </Link>
+        <Link href='/Tentang'>
+          <a>Tentang</a>
+        </Link>
+        <Link href='/faq'>
+          <a>FAQ</a>
+        </Link>
+        <Link href='/login'>
+          <a>Login</a>
+        </Link>
+        <Link href='/signup'>
+          <a>Sign up</a>
+        </Link>
+      </BurgerMenu>
+      {children}
+    </>
+  );
+}
+
+Layout.propTypes = { children: PropTypes.object };
+
+export default Layout;
