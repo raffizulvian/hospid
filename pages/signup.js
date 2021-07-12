3734e26737ded36e72f5fb2f8033ec849787f4e1
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-13 03:18:58 +0700

    feat: create multi step form for signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,46 +1,32 @@
-import { useState } from 'react';
-import ButtonLink from '../components/button/ButtonAction';
+import { useMachine } from '@xstate/react';
+
+import Form, { PersonalDataInput, AuthDataInput } from '../components/form';
 import NavLink from '../components/navigation/NavLink';
 
+import signupStateMachine from '../lib/client/helper/machine';
+
 function Signup() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [current, send] = useMachine(signupStateMachine);
 
   return (
     <>
-      <form className='flex flex-col'>
-        <label htmlFor='username' className='text-gray-700'>
-          Username
-        </label>
-        <input
-          type='text'
-          placeholder='bambang1624'
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-            console.log(username);
-          }}
-          className='mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-purple-600'
-        />
-        <label htmlFor='password' className='text-gray-700 mt-4'>
-          Password
-        </label>
-        <input
-          type='password'
-          name='password'
-          id='password-input'
-          placeholder='********'
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-            console.log(password);
-          }}
-          className='mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-purple-600'
-        />
-        <ButtonLink big className='mt-10'>
-          Selanjutnya
-        </ButtonLink>
-      </form>
+      <Form>
+        {current.matches('personalData') && (
+          <PersonalDataInput
+            defaultValue={current.context}
+            onNext={(value) => send({ type: 'NEXT', ...value })}
+          />
+        )}
+
+        {current.matches('authData') && (
+          <AuthDataInput
+            defaultValue={current.context}
+            onPrev={(value) => send({ type: 'PREV', ...value })}
+            onNext={(value) => send({ type: 'NEXT', ...value })}
+          />
+        )}
+      </Form>
+
       <p className='flex justify-center text-sm text-gray-600 mt-3'>
         Sudah punya akun?&nbsp;
         <NavLink href='/login'>Login</NavLink>
