1ccf3ee789dcad20055efa1f94d8f43b2ca55082
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-07-13 15:35:26 +0700

    feat(component): add disable state for button

--- components/button/ButtonAction.js
+++ components/button/ButtonAction.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
-function ButtonAction({ children, id, onClick, secondary, big, className }) {
+function ButtonAction({ children, id, onClick, secondary, big, disabled, className }) {
   return (
     <button
       id={id}
       onClick={onClick}
+      disabled={disabled}
       className={`inline-block text-center font-semibold rounded-lg border border-solid m-0 ${
         secondary
           ? 'bg-white text-purple-600 border-solid border-purple-600'
@@ -13,7 +14,7 @@ function ButtonAction({ children, id, onClick, secondary, big, className }) {
         big
           ? 'h-14 min-w-[10rem] text-lg px-6 py-3 md:h-[3.875rem] md:min-w-[12.25rem] md:text-xl md:px-7 md:py-4'
           : 'h-10 min-w-[6rem] text-sm px-3 py-2'
-      } ${className}`}>
+      } ${disabled && 'bg-purple-400 text-gray-700'} ${className}`}>
       {children}
     </button>
   );
@@ -25,6 +26,7 @@ ButtonAction.propTypes = {
   onClick: PropTypes.func.isRequired,
   secondary: PropTypes.bool,
   big: PropTypes.bool,
+  disabled: PropTypes.bool,
   className: PropTypes.string,
 };
 
